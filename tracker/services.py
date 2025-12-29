2f1c15fbd8a3e6126e0c45589aec0be0ff0eab89
hoochlef <amin.hochlef@gmail.com>
2025-12-29 14:33:30 +0100

    refactor: refactor services file to make it less complex

--- tracker/services.py
+++ tracker/services.py
@@ -1,30 +1,27 @@
 from paddleocr import PaddleOCR
 
-_ocr_engine = None
+_ocr_engine = PaddleOCR(
+    text_detection_model_name="PP-OCRv5_mobile_det",
+    text_recognition_model_name="PP-OCRv5_mobile_rec",
+    # images may be uploaded sideways or even upside down
+    # from users
+    use_doc_orientation_classify=True,
+    # some medication comes in conic shapes and it needs
+    # to be flattened in order for the model to extract text
+    use_doc_unwarping=True,
+    use_textline_orientation=False,
+    text_det_limit_side_len=960,
+    text_det_limit_type="max",
+)
 
 
 def run_ocr(image_array):
-    global _ocr_engine
-
-    if _ocr_engine is None:
-        _ocr_engine = PaddleOCR(
-            # images may be uploaded sideways or even upside down
-            # from users
-            use_doc_orientation_classify=True,
-            # some medication comes in conic shapes and it needs
-            # to be flattened in order for the model to extract text
-            use_doc_unwarping=True,
-            use_textline_orientation=False,
-            text_det_limit_side_len=960,
-            text_det_limit_type="max",
-        )
-
     result = _ocr_engine.predict(image_array)
 
     if result and len(result) > 0:
         first_page = result[0]
 
-        # Pull the list of recognized strings directly
+        # pull the list of recognized strings directly
         if "rec_texts" in first_page:
             return first_page["rec_texts"]
 
