46bcd49215dc81c8530c7cc70931defb49ab0c6e
hoochlef <amin.hochlef@gmail.com>
2026-01-05 08:22:18 +0100

    feat: add markdown framework to format SLM response into valid html

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,4 @@
+import markdown
 from django.contrib import messages
 from django.shortcuts import redirect, render
 
@@ -15,13 +16,20 @@ def medication_lookup(request):
 
         if form.is_valid():
             image = form.cleaned_data["medication_image"]
+            
             # 1. convert uploaded image to a format that ocr model can work with
             image_array = convert_image_to_nump_array(image)
+
             # 2. run ocr on the image
             ocr_result = run_ocr(image_array)
+
             # 3. pass the ocr result to the model
             slm_response = run_custom_slm(ocr_result)
-            messages.success(request, slm_response)
+
+            # Convert markdown to HTML
+            html_response = markdown.markdown(slm_response)
+
+            messages.success(request, html_response, extra_tags="safe")
             return redirect("tracker:medication_lookup")
 
     else:
